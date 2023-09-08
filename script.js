d8a1eb4ff0c3f9b3d2429a716dfd49ce31dba9cd
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-08 00:23:45 -0500

    Add listener to options during round

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const areas = document.querySelectorAll('.area');
 const startBtn = document.getElementById('start-btn');
 const roundTitle = document.getElementById('round-title');
+const options = document.querySelectorAll('.option');
 let currentRound = 0;
-let totalRounds = 0; 
+let totalRounds = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 startBtn.addEventListener('click', (event) => {
   totalRounds = document.querySelector('select').value;
   enableArea('round-area');
-  updateRound();
+  updateRound('');
 });
+
+options.forEach(option => {
+  option.addEventListener('click', event => {
+    let playerSelection = event.target.getAttribute('id');
+    let computerSelection = getComputerSelection();
+    let result = playRound(playerSelection, computerSelection);
+    updateRound(result);
+  });
+})
 
 function enableArea(selectedArea) {
   areas.forEach(area => {
@@ -20,94 +32,49 @@ function enableArea(selectedArea) {
   });
 }
 
-function updateRound() {
+function updateRound(result) {
   currentRound++;
   if (currentRound <= totalRounds) {
     roundTitle.textContent = `Round ${currentRound} of ${totalRounds}`;
+    if (result.includes('Win')) {
+      playerScore++;
+    } else if (result.includes('Lose')) {
+      computerScore++;
+    }
   } else {
     enableArea('game-over-area');
+    const finalScore = document.createElement('p');
+    finalScore.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    const gameOverArea = document.getElementById('game-over-area');
+    gameOverArea.appendChild(finalScore);
   }
 }
 
 function getComputerSelection() {
-  const computerChoices = [`Rock`, `Paper`, `Scissors`];
+  const computerChoices = [`rock`, `paper`, `scissors`];
   return computerChoices[Math.floor(Math.random() * 3)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-  let result;
-  if (playerSelection == `rock` && computerSelection == `rock`) {
-    result = `It's a Tie!`;
-  } else if (playerSelection == `rock` && computerSelection == `paper`) {
-    result = `You Lose! Paper beats Rock.`;
-  } else if (playerSelection == `rock` && computerSelection == `scissors`) {
-    result = `You Win! Rock beats Scissors.`;
-  } else if (playerSelection == `paper` && computerSelection == `rock`) {
-    result = `You Win! Paper beats Rock.`;
-  } else if (playerSelection == `paper` && computerSelection == `paper`) {
-    result = `It's a Tie!`;
-  } else if (playerSelection == `paper` && computerSelection == `scissors`) {
-    result = `You Lose! Scissors beats Paper.`;
-  } else if (playerSelection == `scissors` && computerSelection == `rock`) {
-    result = `You Lose! Rock beats Scissors.`;
-  } else if (playerSelection == `scissors` && computerSelection == `paper`) {
-    result = `You Win! Scissors beats Paper.`;
-  } else if (playerSelection == `scissors` && computerSelection == `scissors`) {
-    result = `It's a Tie!`;
+  let result = '';
+  if (playerSelection == 'rock' && computerSelection == 'rock') {
+    result = 'It\'s a Tie!';
+  } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    result = 'You Lose! Paper beats Rock.';
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    result = 'You Win! Rock beats Scissors.';
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    result = 'You Win! Paper beats Rock.';
+  } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+    result = 'It\'s a Tie!';
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    result = 'You Lose! Scissors beats Paper.';
+  } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+    result = 'You Lose! Rock beats Scissors.';
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    result = 'You Win! Scissors beats Paper.';
+  } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+    result = 'It\'s a Tie!';
   }
   return result;
 }
-
-function playGame() {  
-  let rounds;
-  let isInvalidRoundsValue = true;
-  let playerScore = 0;
-  let computerScore = 0;
-
-  console.log(`Ladies and gentlemen, welcome to the 74th Hunger Games.`);
-
-  do {
-    console.log(`How many rounds do you want to play?`);
-    rounds = +prompt(`How many rounds do you want to play?`);
-    console.log(`You want to play ${rounds} rounds?`);
-    if (rounds > 0 && rounds <= 5) {
-      isInvalidRoundsValue = false;
-      console.log(`Excellent!`);
-      console.log(`Happy Hunger Games and may the odds be ever in your favor.`);    
-    } else {
-      console.log(`Now, now, didn't we agree to never lie to one another.`);
-      console.log(`Try again, but let's keep it between 1 and 5 this time, shall we?`);
-    }  
-  }
-  while (isInvalidRoundsValue);
-
-  for (let i = 1; i <= rounds; i++) {
-    console.log(`Round ${i} -> Player: ${playerScore}, Computer: ${computerScore}`);
-    console.log("Rock, Paper or Scissors?");
-    let playerSelection = prompt("Rock, Paper or Scissors?");
-    playerSelection =  playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
-    console.log(`You chooose ${playerSelection}.`);
-    let computerSelection = getComputerSelection();
-    console.log(`The computer chooose ${computerSelection}.`);
-    let result = playRound(playerSelection,computerSelection); 
-    if (result.includes(`Win`)) {
-      playerScore++;
-    } else if (result.includes(`Lose`)) {
-      computerScore++;
-    }
-    console.log(result);
-  }
-
-  console.log(`The final score is Player: ${playerScore}, Computer: ${computerScore}`);
-
-  if (playerScore > computerScore) {
-    console.log(`We have a new victor!`);
-  } else if (playerScore < computerScore) {
-    console.log(`Oh, well... It's only death.`);
-  } else {
-    console.log(`It's a tie!`);
-  }
-
-} 
