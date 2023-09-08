c964ad2a14011bfe5a7086cdfca3e7f3e0198792
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-09-08 21:28:19 +0000

    Add score, result and next button

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const areas = document.querySelectorAll('.area');
 const startBtn = document.getElementById('start-btn');
 const roundTitle = document.getElementById('round-title');
 const options = document.querySelectorAll('.option');
+const computerSelectionPara = document.getElementById('computer-selection');
+const result = document.getElementById('result');
+const score = document.getElementById('score');
+const nextBtn = document.getElementById('next-btn');
 let currentRound = 0;
 let totalRounds = 0;
 let playerScore = 0;
@@ -10,17 +14,32 @@ let computerScore = 0;
 startBtn.addEventListener('click', (event) => {
   totalRounds = document.querySelector('select').value;
   enableArea('round-area');
-  updateRound('');
+  startRound();
 });
 
 options.forEach(option => {
   option.addEventListener('click', event => {
+    toggleOptionsBtns(event.target.getAttribute('id'));
     let playerSelection = event.target.getAttribute('id');
     let computerSelection = getComputerSelection();
-    let result = playRound(playerSelection, computerSelection);
-    updateRound(result);
+    playRound(playerSelection, computerSelection);
   });
 })
+
+function toggleOptionsBtns() {
+  options.forEach(option => {
+    if (option.disabled === true) {
+      option.disabled = false;
+    } else {
+      option.disabled = true;
+    }
+  })
+}
+
+nextBtn.addEventListener('click', (event) => {
+  toggleOptionsBtns();
+  startRound();
+});
 
 function enableArea(selectedArea) {
   areas.forEach(area => {
@@ -32,15 +51,13 @@ function enableArea(selectedArea) {
   });
 }
 
-function updateRound(result) {
+function startRound() {
   currentRound++;
   if (currentRound <= totalRounds) {
     roundTitle.textContent = `Round ${currentRound} of ${totalRounds}`;
-    if (result.includes('Win')) {
-      playerScore++;
-    } else if (result.includes('Lose')) {
-      computerScore++;
-    }
+    computerSelectionPara.classList.add('off');
+    result.classList.add('off');
+    nextBtn.classList.add('off');
   } else {
     enableArea('game-over-area');
     const finalScore = document.createElement('p');
@@ -56,25 +73,36 @@ function getComputerSelection() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  let result = '';
+  computerSelectionPara.classList.remove('off');
+  computerSelectionPara.textContent = `The computer picked ${computerSelection}.`;
+  let resultText = '';
   if (playerSelection == 'rock' && computerSelection == 'rock') {
-    result = 'It\'s a Tie!';
+    resultText = 'It\'s a Tie!';
   } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-    result = 'You Lose! Paper beats Rock.';
+    resultText = 'You Lose! Paper beats Rock.';
   } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    result = 'You Win! Rock beats Scissors.';
+    resultText = 'You Win! Rock beats Scissors.';
   } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    result = 'You Win! Paper beats Rock.';
+    resultText = 'You Win! Paper beats Rock.';
   } else if (playerSelection == 'paper' && computerSelection == 'paper') {
-    result = 'It\'s a Tie!';
+    resultText = 'It\'s a Tie!';
   } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-    result = 'You Lose! Scissors beats Paper.';
+    resultText = 'You Lose! Scissors beats Paper.';
   } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-    result = 'You Lose! Rock beats Scissors.';
+    resultText = 'You Lose! Rock beats Scissors.';
   } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    result = 'You Win! Scissors beats Paper.';
+    resultText = 'You Win! Scissors beats Paper.';
   } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-    result = 'It\'s a Tie!';
+    resultText = 'It\'s a Tie!';
   }
-  return result;
+  if (resultText.includes('Win')) {
+    playerScore++;
+  } else if (resultText.includes('Lose')) {
+    computerScore++;
+  }
+  result.classList.remove('off');
+  result.textContent = resultText;
+  score.classList.remove('off');
+  score.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+  nextBtn.classList.remove('off');
 }
